be27e87b9aa4d975fe7c5bd40be8a42065114852
Ricardo <ricardo@yonkolevel.com>
2021-10-18 23:52:08 +0100

    Fix component rendering text

--- components/ErrorView.tsx
+++ components/ErrorView.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { View } from "react-native";
-import { Button, Paragraph } from "react-native-paper";
+import { Button, Paragraph, Text } from "react-native-paper";
+import Layout from './Layout';
 
 interface Props {
   message: string;
@@ -9,10 +10,12 @@ interface Props {
 
 const ErrorView: React.FunctionComponent<Props> = ({ message, onTryAgain }) => {
   return (
+    <Layout>
     <View>
-      <Paragraph>{message}</Paragraph>;
+      <Paragraph>{message}</Paragraph>
       {onTryAgain && <Button onPress={onTryAgain}>Try again</Button>}
     </View>
+    </Layout>
   );
 };
 
